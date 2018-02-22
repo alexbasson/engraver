f577fac8721ea338b72b1ae050d23b22598ca3c6
Alex Basson <alexbasson@gmail.com>
2018-02-21 21:59:01 -0500

    Add linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
   },
+  devtool: 'inline-source-map',
+  entry: './src/index.ts',
   module: {
     rules: [
       {
+        exclude: /node_modules/,
         test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: 'ts-loader'
       }
     ]
-  },
-  resolve: {
-    extensions: [ '.ts', '.js' ]
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
   }
-};
+}
