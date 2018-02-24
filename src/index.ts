686c7cdc60202df629c912d784fda9ee7979080f
Alex Basson <alexbasson@gmail.com>
2018-02-23 21:55:02 -0500

    Introduce WIDTH and HEIGHT constants

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import {App} from './app/app'
+import {Score} from './app/score'
+import {HEIGHT, WIDTH} from './constants'
 
 const main = () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement
+  canvas.height = HEIGHT
+  canvas.width = WIDTH
+
   const ctx = canvas.getContext('2d')
 
   const staff = new App(ctx)
